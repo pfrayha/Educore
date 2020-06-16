dbcefdfa5795b3671bbc3c1ea3334f5ae1452e75
Pedro Frayha <pedro.frayha@gmail.com>
2020-06-16 17:36:05 -0300

    Adds config with local db connection

---
+++ flask/instance/config.py
@@ -0,0 +1,3 @@
+SECRET_KEY = b'\xca\xbbJ\x18:\xf3\x14\xf5\x10\xeb\x8e\xa2\x01\xf5xu,!>\xfa\x1f\xf7\x96N'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:inicial01@localhost:5432/educore_dev'
+FLASKWTF_USE_WTF_I18N = True
