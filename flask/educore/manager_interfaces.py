d892b46902d31a12dd4ec2e056e9348cb5d437af
Pedro Frayha <pedro.frayha@gmail.com>
2020-07-20 20:47:41 -0300

    Creates missing manager interfaces

--- flask/educore/manager_interfaces.py
+++ flask/educore/manager_interfaces.py
@@ -21,3 +21,63 @@ class IReportManager(ABC):
     @abstractmethod
     def generate_report(report_type, **kwargs):
         pass
+
+class IClassManager(ABC):
+    @abstractmethod
+    def get_class_form(id=None):
+        pass
+
+    @abstractmethod
+    def submit_class_form(id=None):
+        pass
+
+    @abstractmethod
+    def get_classes():
+        pass
+
+    @abstractmethod
+    def delete_class(id):
+        pass
+
+    @abstractmethod
+    def populate_class(id):
+        pass
+
+class IGradeManager(ABC):
+    @abstractmethod
+    def get_grade_form(id=None):
+        pass
+
+    @abstractmethod
+    def submit_grade_form(id=None):
+        pass
+
+    @abstractmethod
+    def delete_grade(id):
+        pass
+
+class IPresenceManager(ABC):
+    @abstractmethod
+    def get_presence_form(id=None):
+        pass
+
+    @abstractmethod
+    def submit_presence_form(id=None):
+        pass
+
+class IActivityManager(ABC):
+    @abstractmethod
+    def get_activity_form(id=None):
+        pass
+
+    @abstractmethod
+    def submit_activity_form(id=None):
+        pass
+
+    @abstractmethod
+    def get_activities():
+        pass
+
+    @abstractmethod
+    def delete_activity(id):
+        pass
